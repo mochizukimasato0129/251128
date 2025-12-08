fd87653bb520ea7e5bb22c2fdac11fff9da16af4
mochizukimasato0129 <128865145+mochizukimasato0129@users.noreply.github.com>
2025-12-08 18:18:08 +0900

    change

--- Build/output.framework.js
+++ Build/output.framework.js
@@ -2086,13 +2086,13 @@ var tempI64;
 // === Body ===
 
 var ASM_CONSTS = {
-  4566496: function() {return Module.webglContextAttributes.premultipliedAlpha;},  
- 4566557: function() {return Module.webglContextAttributes.preserveDrawingBuffer;},  
- 4566621: function() {return Module.webglContextAttributes.powerPreference;},  
- 4566679: function() {Module['emscripten_get_now_backup'] = performance.now;},  
- 4566734: function($0) {performance.now = function() { return $0; };},  
- 4566782: function($0) {performance.now = function() { return $0; };},  
- 4566830: function() {performance.now = Module['emscripten_get_now_backup'];}
+  4566480: function() {return Module.webglContextAttributes.premultipliedAlpha;},  
+ 4566541: function() {return Module.webglContextAttributes.preserveDrawingBuffer;},  
+ 4566605: function() {return Module.webglContextAttributes.powerPreference;},  
+ 4566663: function() {Module['emscripten_get_now_backup'] = performance.now;},  
+ 4566718: function($0) {performance.now = function() { return $0; };},  
+ 4566766: function($0) {performance.now = function() { return $0; };},  
+ 4566814: function() {performance.now = Module['emscripten_get_now_backup'];}
 };
 
 
